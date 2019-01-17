46002ce07ea272e8ca98e6baea8006507413fe3d
Kawalya Andrew <andrew58934@gmail.com>
2019-01-17 10:01:28 +0300

    [bg-#3222] mdified the run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import app
 # The down statement of 'if' means only when this script is excuted...
 # directly,the down part will run but when is imported by another..
 # script the down part will not be excecuted
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#    app.run(debug=True)
